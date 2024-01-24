d89eb57da63aade54fcb9ca5a3cf93f9b0145e5e
dadaguru <dadaguruweb@gmail.com>
2024-01-24 08:48:40 +0530

    [yj] Added the full working version

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,34 @@
-export default function Page() {
-  return <p>Customers Page</p>;
+import { lusitana } from '@/app/ui/fonts';
+import Search from '@/app/ui/search';
+import { CreateInvoice } from '@/app/ui/invoices/buttons';
+import { Suspense } from 'react';
+import { CustomersTableSkeleton  } from '@/app/ui/skeletons';
+import Table from '@/app/ui/customers/table';
+import Pagination from '@/app/ui/invoices/pagination';
+import { fetchFilteredCustomers } from '@/app/lib/data';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Customers',
+};
+
+export default async function Page({
+  searchParams,
+}: {
+  searchParams?: {
+    query?: string;
+    //page?: string;
+  };
+}) {
+  const query = searchParams?.query || '';
+  //const currentPage = Number(searchParams?.page) || 1;
+  //const totalPages = await fetchFilteredCustomers(query);
+  const customers = await fetchFilteredCustomers(query);
+
+  return <div className="w-full">    
+    <Suspense key={query} fallback={<CustomersTableSkeleton />}>
+      <Table customers={customers} />
+    </Suspense>
+    
+  </div>;
 }
